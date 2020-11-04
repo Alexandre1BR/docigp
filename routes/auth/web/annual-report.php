170588554175b418ff424c8b5e2408d0c46c22da
BMasquio <brunomasquio@gmail.com>
2020-11-04 18:36:09 +0000

    Fix

--- routes/auth/web/annual-report.php
+++ routes/auth/web/annual-report.php
@@ -2,7 +2,4 @@
 
 Route::group(['prefix' => '/annual-report'], function () {
     Route::get('/', 'AnnualReport@index')->name('annual-report.index');
-    Route::get('/cost-center', 'AnnualReport@costCenterTable')->name(
-        'annual-report.cost-center'
-    );
 });
