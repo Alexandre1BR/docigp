ba510988b1e97ba61512dece3082d8e9dc073dc6
RobsonMacedo <robson.s.macedo@gmail.com>
2021-05-31 18:55:40 +0000

    ApllicationTest

--- resources/views/admin/annual-reports/index.blade.php
+++ resources/views/admin/annual-reports/index.blade.php
@@ -16,7 +16,8 @@
                         <div class="form-row justify-content-center">
                             <div class="form-group col-md-2">
                                 <label for="year">Selecione o ano </label>
-                                <input class='form-control' type="number" name="year" id="" min='2019' step='1'>
+                                <input class='form-control' dusk="CongressmanReport" type="number" name="year" id=""
+                                    min='2019' step='1'>
                             </div>
                         </div>
                         <div class="form-row justify-content-center">
@@ -31,7 +32,8 @@
                             </div>
                         </div>
 
-                        <button type="submit" class='btn btn-primary btn-sm'>Baixar pdf</button>
+                        <button type="submit" class='btn btn-primary btn-sm' dusk="submit-CongressmanReport">Baixar
+                            pdf</button>
                     </form>
                 </div>
             </div>
@@ -44,10 +46,11 @@
                         <div class="form-row justify-content-center">
                             <div class="form-group col-md-2">
                                 <label for="year">Selecione o ano </label>
-                                <input class='form-control' type="number" name="year" id="" min='2019' step='1'>
+                                <input class='form-control' dusk="Report" type="number" name="year" id="" min='2019'
+                                    step='1'>
                             </div>
                         </div>
-                        <button type="submit" class='btn btn-primary btn-sm'>Baixar pdf</button>
+                        <button type="submit" dusk="submit-Report" class='btn btn-primary btn-sm'>Baixar pdf</button>
                     </form>
                     <br>
                 </div>
