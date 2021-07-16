510092408f790254c37832b97b031fb11976d488
BMasquio <brunomasquio@gmail.com>
2021-07-16 14:55:49 -0300

    Fix test

--- tests/Browser/Pages/ApplicationTest.php
+++ tests/Browser/Pages/ApplicationTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Tests\Browser\Pages;
 
 use App\Models\Entry;
@@ -27,7 +26,6 @@ class ApplicationTest extends DuskTestCase
     private static $randomEntry;
     private static $newEntryDocument;
 
-
     public function createAdminstrator()
     {
         $user = User::factory()->create();
@@ -38,40 +36,38 @@ class ApplicationTest extends DuskTestCase
     public function init()
     {
         static::$randomCongressman = app(Congressmen::class)
-        ->randomElement()
-        ->toArray();
+            ->randomElement()
+            ->toArray();
 
         static::$randomEntry = app(EntryTypes::class)
-        ->randomElement()
-        ->toArray();
+            ->randomElement()
+            ->toArray();
 
-        static::$newEntriesRaw =  Entry::factory()->make();
+        static::$newEntriesRaw = Entry::factory()->make();
 
         static::$randomProvider = Provider::find(static::$newEntriesRaw['provider_id']);
         static::$randomEntryType = EntryType::find(static::$newEntriesRaw['entry_type_id']);
 
         static::$provider = Provider::factory()->make();
 
-        static::$newEntryDocument = EntryDocument::factory()->make();
-
+        //        static::$newEntryDocument = EntryDocument::factory()->make();
     }
 
     public function documentNumber($newEntriesRaw)
     {
         static::$document = $newEntriesRaw['document_number'];
         if (is_null(static::$document)) {
-            return "0000";
+            return '0000';
         } else {
             return $newEntriesRaw['document_number'];
         }
     }
 
     /**
-        * @test
-        * @group testInsert
-        * @group link
-        */
-
+     * @test
+     * @group testInsert
+     * @group link
+     */
 
     public function testInsertEntry()
     {
@@ -98,17 +94,17 @@ class ApplicationTest extends DuskTestCase
                 ->assertSee('Prestação de Contas')
                 ->type('@filter_input', $randomCongressman['name'])
                 ->pause(2000)
-                ->press('@congressman-'.$randomCongressman['id'])
+                ->press('@congressman-' . $randomCongressman['id'])
                 ->waitForText('Orçamento mensal')
                 ->pause(4000)
-//                ->waitFor('@percentageButton')
+                //                ->waitFor('@percentageButton')
                 ->screenshot('1-Budget')
                 ->script('$("tr[class=\'cursor-pointer\']").click()');
-//                ->pause(2000)
-//                ->script('document.querySelector("button[dusk=\'percentageButton\']").click();');
-//            $inside_user
-//                ->type('@input-percentage', $rand)
-//                ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
+            //                ->pause(2000)
+            //                ->script('document.querySelector("button[dusk=\'percentageButton\']").click();');
+            //            $inside_user
+            //                ->type('@input-percentage', $rand)
+            //                ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
             $inside_user
                 ->waitForText('Lançamentos', 10)
                 ->screenshot('2-Entry')
@@ -117,27 +113,24 @@ class ApplicationTest extends DuskTestCase
                 ->type('#date', $newEntriesRaw['date']->format('d/m/Y'))
                 ->type('@dusk_value', $newEntriesRaw['value'])
                 ->click('.vs__selected-options');
-            $inside_user
-                ->elements('.vs__dropdown-option')[0]->click();
+            $inside_user->elements('.vs__dropdown-option')[0]->click();
             $inside_user
                 ->type('#document_number', $document)
                 ->type('#object', $newEntriesRaw['object'])
                 ->type('#provider_cpf_cnpj', $provider['cpf_cnpj'])
                 ->type('#to', $provider['name'])
-                ->script('$("div[id=\'cost_center_id\']").children().children()[0].setAttribute(\'class\', \'vs__selected-options1\')');
-            $inside_user
-                ->click('.vs__selected-options1');
-            $inside_user
-                ->elements('.vs__dropdown-option')[0]->click();
+                ->script(
+                    '$("div[id=\'cost_center_id\']").children().children()[0].setAttribute(\'class\', \'vs__selected-options1\')'
+                );
+            $inside_user->click('.vs__selected-options1');
+            $inside_user->elements('.vs__dropdown-option')[0]->click();
             $inside_user
                 ->pause(2000)
                 ->screenshot('4-EntryForm-Filled')
                 ->press('Gravar');
-            $inside_user
-                ->pause(6000)
-                ->script('$("tr[dusk=\'entrie\']").click()');
-//                ->waitFor('@entrie',8)
-//                ->click('@entrie')
+            $inside_user->pause(6000)->script('$("tr[dusk=\'entrie\']").click()');
+            //                ->waitFor('@entrie',8)
+            //                ->click('@entrie')
             $inside_user
                 ->waitForText('Documentos')
                 ->click('@newEntryDocument')
@@ -150,25 +143,19 @@ class ApplicationTest extends DuskTestCase
             $inside_user
                 ->screenshot('6-Comment')
                 ->script('$("button[dusk=\'newEntryComment\']").click()');
-            $inside_user
-                ->type('#text', 'teste')
-                ->press('Gravar');
+            $inside_user->type('#text', 'teste')->press('Gravar');
             $inside_user
                 ->waitFor('@editComment', 8)
                 ->script('$("button[dusk=\'editComment\']").click()');
-            $inside_user
-                ->type('#text', $rand)
-                ->press('Gravar');
+            $inside_user->type('#text', $rand)->press('Gravar');
             $inside_user
                 ->pause(1000)
                 ->waitForText($rand)
                 ->assertSee($rand)
                 ->screenshot('7-Comment-Edited')
                 ->script('window.scrollTo(0 , document.body.scrollHeight);');
-            $inside_user
-                ->script('$("button[dusk=\'deleteComment\']").click()');
-            $inside_user
-                ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
+            $inside_user->script('$("button[dusk=\'deleteComment\']").click()');
+            $inside_user->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
             $inside_user
                 ->screenshot('8-Comment-Deleted')
                 ->pause(2000)
@@ -176,15 +163,11 @@ class ApplicationTest extends DuskTestCase
             $inside_user
                 ->screenshot('9-verify_document')
                 ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
-            $inside_user
-                ->pause(2000)
-                ->script('$("button[dusk=\'analize_document\']").click()');
+            $inside_user->pause(2000)->script('$("button[dusk=\'analize_document\']").click()');
             $inside_user
                 ->screenshot('10-analize_document')
                 ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
-            $inside_user
-                ->pause(2000)
-                ->script('$("button[dusk=\'verify_entry_button\']").click()');
+            $inside_user->pause(2000)->script('$("button[dusk=\'verify_entry_button\']").click()');
             $inside_user
                 ->screenshot('11-verify_entry_button')
                 ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
@@ -216,15 +199,13 @@ class ApplicationTest extends DuskTestCase
             $inside_user
                 ->screenshot('15-publish_budget_button')
                 ->script('$("button[class=\'swal2-confirm swal2-styled\']").click()');
-            $inside_user
-                ->waitUntilMissing('Salvo com sucesso')
-                ->pause(2000);
+            $inside_user->waitUntilMissing('Salvo com sucesso')->pause(2000);
             $outside_user
                 ->logout()
                 ->visit('/transparencia#/')
                 ->waitforText('Prestação de Contas')
                 ->type('@filter_input', $randomCongressman['name'])
-                ->waitFor('@congressman-'.$randomCongressman['id'])
+                ->waitFor('@congressman-' . $randomCongressman['id'])
                 ->assertSee($randomCongressman['nickname'])
                 ->waitForText('Aderiu?')
                 ->assertSee('sim')
