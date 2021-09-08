903ba53b4f1e8d75275d8cad1769ada12f5ea50b
ricardocamargo07 <53793788+ricardocamargo07@users.noreply.github.com>
2021-09-08 16:06:02 -0300

    annual-reports como está no repositorio como pedido

--- routes/auth/web/annual-reports.php
+++ routes/auth/web/annual-reports.php
@@ -1,10 +1,9 @@
 <?php
 
 use App\Http\Controllers\Web\Admin\AnnualReport as AnnualReports;
-use App\Http\Controllers\Web\Admin\AnnualReport as AnnualReport;
 
 Route::group(['prefix' => '/annual-report'], function () {
-    Route::post('/', [AnnualReport::class,'generate'])->name('annual-reports.generate');
-    Route::get('/', [AnnualReport::class,'index'])->name('annual-reports.index');
-    Route::post('/generateGeneral', [AnnualReport::class,'generateGeneral'])->name('general-annual-reports.generate');
+    Route::post('/', [AnnualReports::class,'generate'])->name('annual-reports.generate');
+    Route::get('/', [AnnualReports::class,'index'])->name('annual-reports.index');
+    Route::post('/generateGeneral', [AnnualReports::class,'generateGeneral'])->name('general-annual-reports.generate');
 });
