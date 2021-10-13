75ba33c40147cd369077bce41313ee2ab2a7fefb
marcoscoe <marcoscoe9@gmail.com>
2021-10-13 20:17:47 +0000

    logs de atividade

--- resources/views/livewire/audits/index.blade.php
+++ resources/views/livewire/audits/index.blade.php
@@ -1,58 +1,42 @@
-
-
 <div>
     <div class="card card-default">
         <div class="card-header">
             <div class="row">
-                <div class="col-md-3">
+                <div class=" form-group col-md-4">
                     <h4 class="mb-0">Logs de atividades</h4>
                 </div>
 
 
 
-                <div class="col-md-9">
-                        <div class="form-group pull-right">
-                            <div class="row d-flex justify-content-end">
-                                <div class="col-xs-12 d-flex justify-content-end">
-                                    <label class="form-check-label d-inline-block" for="user_id">Usuário</label>
-                                    <div class="input-group">
-                                        <div wire:ignore>
-                                            <select data-allow-clear="true"
-                                                    id="user_id"
-                                                    name="user_id"
-                                                    class="select2" data-placeholder="Selecione um usuário">
-                                                <option value=""></option>
-                                                @foreach($users as $user)
-                                                    <option value="{{$user->id}}">{{$user->name}}</option>
-                                                @endForEach
+                <div class="col-md-8">
+                    <form>
+                        <div class="form-row justify-content-end">
+                            <div class="form-group col-lg-3 col-md-3" wire:ignore>
+                                <label class="col-form-label" for="user_id">Usuário:</label>
+                                <select data-allow-clear="true" id="user_id" name="user_id" class="select2" data-placeholder="Selecione um usuário">
+                                    <option value=""></option>
+                                    @foreach($users as $user)
+                                    <option value="{{$user->id}}">{{$user->name}}</option>
+                                    @endForEach
+                                </select>
+                            </div>
 
-                                            </select>
-                                        </div>
 
-{{--                                        <div class="input-group">--}}
-{{--                                            <input dusk="search-input" class="form-control" name="search" wire:model.debounce.500ms="searchString" placeholder="Pesquisar">--}}
-{{--                                        </div>--}}
+                            {{-- <div class="input-group">--}}
+                            {{-- <input dusk="search-input" class="form-control" name="search" wire:model.debounce.500ms="searchString" placeholder="Pesquisar">--}}
+                            {{-- </div>--}}
 
-                                        <div class="">
-                                            <label for="created_at_start" class="col-form-label">Data<br> De:</label>
-                                            <input id="created_at_start"
-                                                   wire:model="created_at_start"
-                                                   class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                                   name="created_at_start" value="{{$created_at_start ?? old("created_at_start")}}"
-                                                   type="date">
-                                            <label for="created_at_end" class="col-form-label"> Até: </label>
-                                            <input id="created_at_end"
-                                                   wire:model="created_at_end"
-                                                   class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                                   name="created_at_end" value="{{$created_at_end ?? old("created_at_end")}}" type="date">
-                                        </div>
 
-                                    </div>
-                                </div>
+                            <div class="form-group col-lg-3 col-md-3">
+                                <label for="created_at_start" class="col-form-label">Data de:</label>
+                                <input id="created_at_start" wire:model="created_at_start" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="created_at_start" value="{{$created_at_start ?? old("created_at_start")}}" type="date">
+                            </div>
+                            <div class="form-group col-lg-3 col-md-3">
+                                <label for="created_at_end" class="col-form-label"> Até: </label>
+                                <input id="created_at_end" wire:model="created_at_end" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="created_at_end" value="{{$created_at_end ?? old("created_at_end")}}" type="date">
                             </div>
                         </div>
-
-
+                    </form>
                 </div>
             </div>
         </div>
@@ -60,89 +44,89 @@
         <div class="card-body">
             <table id="auditsTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
-                <tr>
-                    <th>Data da ação</th>
-                    <th>Usuário</th>
-                    <th>Ação</th>
-                    <th>Entidade modificada</th>
-                    <th>Referência</th>
-                    <th>Valores antigos</th>
-                    <th>Valores novos</th>
-                    <th>Url/Rota</th>
+                    <tr class="text-center">
+                        <th class="align-middle">Data da ação</th>
+                        <th class="align-middle">Usuário</th>
+                        <th class="align-middle">Ação</th>
+                        <th class="align-middle">Entidade modificada</th>
+                        <th class="align-middle">Referência</th>
+                        <th class="align-middle">Valores antigos</th>
+                        <th class="align-middle">Valores novos</th>
+                        <th class="align-middle">Url/Rota</th>
 
-                </tr>
+                    </tr>
                 </thead>
 
                 @forelse ($audits as $audit)
-                    <tr>
-                        <td>
-                            {{ $audit->formatted_created_at }}
-                        </td>
-                        <td>
-                            {{ $audit->user->name ?? '' }}
-                            <br/>
-                            {{ $audit->user->email ?? '' }}
-                        </td>
-                        <td>
-                            {{ $audit->activity ?? ''}}
-                        </td>
-                        <td>
-                            {{ __($audit->entity) ?? '' }}
-                            <br/>
-                            (id: {{$audit->auditable_id ?? ''}})
-                        </td>
+                <tr>
+                    <td>
+                        {{ $audit->formatted_created_at }}
+                    </td>
+                    <td>
+                        {{ $audit->user->name ?? '' }}
+                        <br />
+                        {{ $audit->user->email ?? '' }}
+                    </td>
+                    <td>
+                        {{ $audit->activity ?? ''}}
+                    </td>
+                    <td>
+                        {{ __($audit->entity) ?? '' }}
+                        <br />
+                        (id: {{$audit->auditable_id ?? ''}})
+                    </td>
 
-                        <td>
-                            <table>
-                                @forEach($audit->reference as $item)
-                                    <tr>
+                    <td>
+                        <table>
+                            @forEach($audit->reference as $item)
+                            <tr>
 
-                                        <td>
-                                            {{$item['field'] ?? ''}}
-                                        </td>
-                                        <td>
-                                            {{$item['value'] ?? 'null'}}
-                                        </td>
+                                <td>
+                                    {{$item['field'] ?? ''}}
+                                </td>
+                                <td>
+                                    {{$item['value'] ?? 'null'}}
+                                </td>
 
-                                    </tr>
-                                @endForEach
-                            </table>
+                            </tr>
+                            @endForEach
+                        </table>
 
 
 
-                        </td>
+                    </td>
 
-                        <td>
-                            <ul class="list-group list-group-flush">
-                                @forEach($audit->old_values as $key => $value)
-                                    <li class="list-group-item">
-                                        {{__($key)}} => {{$value ?? 'null'}}
-                                    </li>
-                                @endForEach
-                            </ul>
-                        </td>
-                        <td>
+                    <td>
+                        <ul class="list-group list-group-flush">
+                            @forEach($audit->old_values as $key => $value)
+                            <li class="list-group-item">
+                                {{__($key)}} => {{$value ?? 'null'}}
+                            </li>
+                            @endForEach
+                        </ul>
+                    </td>
+                    <td>
 
-                            <ul class="list-group list-group-flush">
-                                @forEach($audit->new_values as $key => $value)
-                                    <li class="list-group-item">
-                                        {{__($key)}} => {{$value ?? 'null'}}
-                                    </li>
-                                @endForEach
+                        <ul class="list-group list-group-flush">
+                            @forEach($audit->new_values as $key => $value)
+                            <li class="list-group-item">
+                                {{__($key)}} => {{$value ?? 'null'}}
+                            </li>
+                            @endForEach
 
-                            </ul>
-                        </td>
+                        </ul>
+                    </td>
 
-                        <td>
-                            {{ $audit->url ?? ''}}
-                            <br/>
-                            {{ $audit->route_name ?? ''}}
-                        </td>
-                    </tr>
+                    <td>
+                        {{ $audit->url ?? ''}}
+                        <br />
+                        {{ $audit->route_name ?? ''}}
+                    </td>
+                </tr>
 
 
                 @empty
-                    <p>Nenhum dado encontrado</p>
+                <p>Nenhum dado encontrado</p>
                 @endforelse
 
                 {{ $audits->links() }}
